475dfb8858b91d2a979f5a10501f71168148eb3b
JahanzebBalcoh-Personal <amjahanzaib15@gmail.com>
2026-05-11 18:38:37 +0500

    Updated notification sound and background push support

--- sw.js
+++ sw.js
@@ -1,9 +1,32 @@
-const CACHE_NAME = 'sultans-admin-v5';
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBfhbjD0b8UaISn1QrK6E-Ci5Yr7HcUTzA",
+  authDomain: "sultans-cricket.firebaseapp.com",
+  projectId: "sultans-cricket",
+  storageBucket: "sultans-cricket.firebasestorage.app",
+  messagingSenderId: "975861366304",
+  appId: "1:975861366304:web:6bfef2fc3e3b01d0284645"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: './icon.jpg'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+const CACHE_NAME = 'sultans-admin-v6';
 const ASSETS = [
   './',
   './index.html',
-  './css/style.css',
-  './js/app.js',
   './icon.jpg'
 ];
 
