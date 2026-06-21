278752252981f1cbbf130d978d7d5c1096078ecd
JahanzebBalcoh-Personal <amjahanzaib15@gmail.com>
2026-06-22 03:12:18 +0500

    Fix: Remove service worker cache to force latest code updates

--- sw.js
+++ sw.js
@@ -23,34 +23,21 @@ messaging.onBackgroundMessage((payload) => {
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-const CACHE_NAME = 'sultans-admin-v6';
-const ASSETS = [
-  './',
-  './index.html',
-  './icon.jpg'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+// SELF-DESTRUCT LOGIC FOR CACHES
+self.addEventListener('install', function(e) {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
+self.addEventListener('activate', function(e) {
   e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
+    caches.keys().then(function(names) {
+      return Promise.all(names.map(function(name) { return caches.delete(name); }));
+    }).then(function() {
+      return self.clients.claim();
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', function(e) {
+  e.respondWith(fetch(e.request));
 });
